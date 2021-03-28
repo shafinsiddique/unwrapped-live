2594153d869290845a606e7f80fce54abc688d78
shafinsiddique <siddiqueshafin20@gmail.com>
2021-03-27 22:22:58 -0400

    moved secrets to env file

--- server/src/unwrapped-live/main.go
+++ server/src/unwrapped-live/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"github.com/pkg/errors"
 	"github.com/rs/cors"
 	"github.com/sirupsen/logrus"
@@ -17,41 +18,43 @@ import (
 )
 
 var logger *logrus.Logger
+var CLIENT_SECRET string
+var JWT_SECRET string
 
 const (
-	CODE = "code"
-	CLIENT_ID = "0fcccb78740a42dab96c20f4ebb9dbae"
-	CLIENT_SECRET = "90a72ac119f14b7994b7ed4bb77373bc" // RESET, AND SWITCH TOS ECURE FORM AFTER.
-	EXCHANGE_TOKEN_LINK = "https://accounts.spotify.com/api/token"
+	CODE                      = "code"
+	CLIENT_ID                 = "0fcccb78740a42dab96c20f4ebb9dbae"
+	EXCHANGE_TOKEN_LINK       = "https://accounts.spotify.com/api/token"
 	CONTENT_TYPE_FORM_ENCODED = "application/x-www-form-urlencoded"
-	GRANT_TYPE = "grant_type"
-	AUTHORIZATION_CODE = "authorization_code"
-	REDIRECT_URI_PARAM = "redirect_uri"
-	REDIRECT_URI = "http://localhost:3000/redirect"
-	CONTENT_TYPE_HEADER = "Content-Type"
-	CLIENT_ID_KEY = "client_id"
-	CLIENT_SECRET_KEY = "client_secret"
-	ACCESS_TOKEN = "access_token"
-	REFRESH_TOKEN = "refresh_token"
-	TEMP_ACCESS_SECRET = "my-oauth-secret-secure-123" // will change after swithcin gto env file.
-	APPLICATION_JSON = "application/json"
-	JWT = "jwt"
-	SPOTIFY_API_BASE = "https://api.spotify.com/v1"
-	PROFILE = "profile"
-	PERSONALIZATION = "personalization"
-	TRACKS = "tracks"
-	ARTISTS = "artists"
-	RESPONSE_LIMIT = "5"
+	GRANT_TYPE                = "grant_type"
+	AUTHORIZATION_CODE        = "authorization_code"
+	REDIRECT_URI_PARAM        = "redirect_uri"
+	REDIRECT_URI              = "http://localhost:3000/redirect"
+	CONTENT_TYPE_HEADER       = "Content-Type"
+	CLIENT_ID_KEY             = "client_id"
+	CLIENT_SECRET_KEY         = "client_secret"
+	ACCESS_TOKEN              = "access_token"
+	REFRESH_TOKEN             = "refresh_token"
+	APPLICATION_JSON          = "application/json"
+	JWT                       = "jwt"
+	SPOTIFY_API_BASE          = "https://api.spotify.com/v1"
+	PROFILE                   = "profile"
+	PERSONALIZATION           = "personalization"
+	TRACKS                    = "tracks"
+	ARTISTS                   = "artists"
+	RESPONSE_LIMIT            = "5"
+	SPOTIFY_CLIENT_SECRET_KEY = "SPOTIFY_CLIENT_SECRET"
+	JWT_SECRET_KEY            = "JWT_SECRET"
 )
 
-func logRequest(r *http.Request)  {
-	logger.WithFields(logrus.Fields{"ip":r.RemoteAddr, "method":r.Method, "host":r.Host,
-		"url":r.URL}).Info("")
+func logRequest(r *http.Request) {
+	logger.WithFields(logrus.Fields{"ip": r.RemoteAddr, "method": r.Method, "host": r.Host,
+		"url": r.URL}).Info("")
 }
 
 func getAuthResponse(rawResponse *http.Response) *AuthResponse {
 	decoder := json.NewDecoder(rawResponse.Body)
-	authResponse := &AuthResponse{ }
+	authResponse := &AuthResponse{}
 	decoder.Decode(authResponse) // can ignore error cause Spotify API will send it in this format.
 	return authResponse
 }
@@ -61,7 +64,7 @@ func getJwt(authResponse *AuthResponse) string {
 	jwtClaims[ACCESS_TOKEN] = authResponse.AccessToken
 	jwtClaims[REFRESH_TOKEN] = authResponse.RefreshToken
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwtClaims)
-	tokenStr, _ := token.SignedString([]byte(TEMP_ACCESS_SECRET))
+	tokenStr, _ := token.SignedString([]byte(JWT_SECRET))
 	return tokenStr
 }
 
@@ -72,8 +75,8 @@ func sendJson(w http.ResponseWriter, data interface{}) {
 
 }
 
-func getAccessToken(code string, grantType string) (*AuthResponse, int,  error) {
-	logger.WithFields(logrus.Fields{GRANT_TYPE:grantType}).Info("Attempting to fetch access token.")
+func getAccessToken(code string, grantType string) (*AuthResponse, int, error) {
+	logger.WithFields(logrus.Fields{GRANT_TYPE: grantType}).Info("Attempting to fetch access token.")
 	data := url.Values{}
 	data.Set(GRANT_TYPE, grantType)
 	codeType := CODE
@@ -91,9 +94,9 @@ func getAccessToken(code string, grantType string) (*AuthResponse, int,  error) 
 		logger.Error("Error trying to get Access Token from Spotify. Check Network Connectivity.")
 		return nil, 0, err
 	} else if resp.StatusCode != http.StatusOK {
-		logger.WithFields(logrus.Fields{"status code":resp.StatusCode}).
+		logger.WithFields(logrus.Fields{"status code": resp.StatusCode}).
 			Info("Non 200 Response when trying to " +
-			"fetch access token.")
+				"fetch access token.")
 
 		return nil, resp.StatusCode, nil
 	}
@@ -116,7 +119,7 @@ func sendJwt(code string, grantType string, w http.ResponseWriter) {
 
 	} else {
 		jwtToken := getJwt(token)
-		sendJson(w, map[string]string{JWT:jwtToken})
+		sendJson(w, map[string]string{JWT: jwtToken})
 		logger.Info("Successfully fetched Access Token and responded with JWT.")
 	}
 }
@@ -137,28 +140,28 @@ func tryParseJwt(r *http.Request) (jwt.MapClaims, error) {
 		logger.Info("received bad request without 'jwt'.")
 		return nil, errors.New("missing required field 'jwt'")
 	} else {
-			jwt_token := body.Jwt
-			claims := jwt.MapClaims{}
-			token, err := jwt.ParseWithClaims(jwt_token, claims, func(token *jwt.Token) (interface{}, error) {
-			return []byte(TEMP_ACCESS_SECRET), nil
+		jwt_token := body.Jwt
+		claims := jwt.MapClaims{}
+		token, err := jwt.ParseWithClaims(jwt_token, claims, func(token *jwt.Token) (interface{}, error) {
+			return []byte(JWT_SECRET), nil
 		})
-			if err != nil || !token.Valid {
+
+		if err != nil || !token.Valid {
 			return nil, err
 		} else {
 			return claims, nil
 		}
-		}
+	}
 }
 
-
-func tryGetDataFromSpotify(url string, token string) (map[string]interface{},int,  error) {
+func tryGetDataFromSpotify(url string, token string) (map[string]interface{}, int, error) {
 	client := &http.Client{}
 	req, _ := http.NewRequest(http.MethodGet, url, nil)
-	req.Header.Add("Authorization","Bearer " + token)
+	req.Header.Add("Authorization", "Bearer "+token)
 	resp, err := client.Do(req) // only possible error should be networkl connectivity problems, send a internal
 	// server error to client.
 	if err != nil {
-		logger.WithFields(logrus.Fields{"url":url, "error":err}).Error(err)
+		logger.WithFields(logrus.Fields{"url": url, "error": err}).Error(err)
 		return nil, 0, err
 	}
 
@@ -173,36 +176,36 @@ func getData(w http.ResponseWriter, r *http.Request) {
 	claims, err := tryParseJwt(r)
 	if err != nil || claims == nil {
 		w.WriteHeader(http.StatusBadRequest)
-	} else{
+	} else {
 		accessToken := claims[ACCESS_TOKEN].(string)
-		profile, stat1, err1 := tryGetDataFromSpotify(SPOTIFY_API_BASE + "/me", accessToken)
-		artists, stat2, err2 := tryGetDataFromSpotify(SPOTIFY_API_BASE + "/me/top/artists?limit="+RESPONSE_LIMIT,
+		profile, stat1, err1 := tryGetDataFromSpotify(SPOTIFY_API_BASE+"/me", accessToken)
+		artists, stat2, err2 := tryGetDataFromSpotify(SPOTIFY_API_BASE+"/me/top/artists?limit="+RESPONSE_LIMIT,
 			accessToken)
-		tracks, stat3, err3 := tryGetDataFromSpotify(SPOTIFY_API_BASE + "/me/top/tracks?limit="+RESPONSE_LIMIT,
+		tracks, stat3, err3 := tryGetDataFromSpotify(SPOTIFY_API_BASE+"/me/top/tracks?limit="+RESPONSE_LIMIT,
 			accessToken)
 
 		if err1 != nil || err2 != nil || err3 != nil {
 			w.WriteHeader(http.StatusServiceUnavailable)
-		} else if stat1 != http.StatusOK  {
-			logger.WithFields(logrus.Fields{"status_code":stat1, "type":PROFILE}).
+		} else if stat1 != http.StatusOK {
+			logger.WithFields(logrus.Fields{"status_code": stat1, "type": PROFILE}).
 				Error("Non 200 Status Code when " +
-				"fetching data from spotify.")
+					"fetching data from spotify.")
 			w.WriteHeader(stat1)
 		} else if stat2 != http.StatusOK {
-			logger.WithFields(logrus.Fields{"status_code":stat2, "type":ARTISTS}).
+			logger.WithFields(logrus.Fields{"status_code": stat2, "type": ARTISTS}).
 				Error("Non 200 Status Code when " +
 					"fetching data from spotify.")
 			w.WriteHeader(stat2)
 		} else if stat3 != http.StatusOK {
-			logger.WithFields(logrus.Fields{"status_code":stat3, "type":TRACKS}).
+			logger.WithFields(logrus.Fields{"status_code": stat3, "type": TRACKS}).
 				Error("Non 200 Status Code when " +
 					"fetching data from spotify.")
 			w.WriteHeader(stat3)
 		} else {
 			tracksList := tracks["items"]
 			artistsList := artists["items"]
-			data := map[string]map[string]interface{}{PROFILE:profile,
-				PERSONALIZATION:{TRACKS:tracksList, ARTISTS:artistsList}}
+			data := map[string]map[string]interface{}{PROFILE: profile,
+				PERSONALIZATION: {TRACKS: tracksList, ARTISTS: artistsList}}
 			sendJson(w, data)
 			logger.Info("Successfully fetched all required data, responded to client.")
 		}
@@ -214,10 +217,10 @@ func refresh(w http.ResponseWriter, r *http.Request) {
 	claims, err := tryParseJwt(r)
 	if err != nil || claims == nil {
 		w.WriteHeader(http.StatusBadRequest)
+	} else {
+		refresh_token := claims[REFRESH_TOKEN].(string)
+		sendJwt(refresh_token, REFRESH_TOKEN, w)
 	}
-
-	refresh_token := claims[REFRESH_TOKEN].(string)
-	sendJwt(refresh_token, REFRESH_TOKEN, w)
 }
 
 func initLogger() {
@@ -227,7 +230,7 @@ func initLogger() {
 	logger.SetFormatter(&logrus.TextFormatter{})
 	logger.SetFormatter(customFormatter)
 	path, _ := os.Getwd()
-	logFile, err := os.OpenFile(path + "/logs/logs.txt", os.O_APPEND | os.O_CREATE | os.O_WRONLY, 0755)
+	logFile, err := os.OpenFile(path+"/logs/logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
 	if err == nil {
 		logger.SetOutput(io.MultiWriter(os.Stderr, logFile))
 	} else {
@@ -235,13 +238,24 @@ func initLogger() {
 	}
 }
 
+func initEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		logger.Fatal("Error loading .env file")
+	} else {
+		CLIENT_SECRET = os.Getenv(SPOTIFY_CLIENT_SECRET_KEY)
+		JWT_SECRET = os.Getenv(JWT_SECRET_KEY)
+	}
+}
+
 func main() {
 	initLogger()
+	initEnv()
 	addr := "127.0.0.1:5000"
 	router := mux.NewRouter()
 	router.HandleFunc("/auth/{code}", authorize).Methods(http.MethodGet)
 	router.HandleFunc("/data", getData).Methods(http.MethodPost)
-	router.HandleFunc("/refresh",refresh).Methods(http.MethodPost)
+	router.HandleFunc("/refresh", refresh).Methods(http.MethodPost)
 	c := cors.New(cors.Options{
 		AllowedOrigins:   []string{"*"},
 		AllowedMethods:   []string{http.MethodGet, http.MethodPost},
@@ -249,8 +263,8 @@ func main() {
 	})
 
 	corsHandler := c.Handler(router)
-	server := &http.Server{Handler: corsHandler, Addr: addr,WriteTimeout: 15*time.Second,
-		ReadTimeout: 15*time.Second}
+	server := &http.Server{Handler: corsHandler, Addr: addr, WriteTimeout: 15 * time.Second,
+		ReadTimeout: 15 * time.Second}
 	logger.Info("Starting unwrapped-live server on address " + addr)
 	log.Fatal(server.ListenAndServe())
 
